e302ca6444eda16ba5308e02b641ee7ad3c26382
arikanmstf <arikanmstf@gmail.com>
2023-09-27 19:14:10 +0300

    add form

--- src/app/modules/manage-restaurant/manage-restaurant.module.ts
+++ src/app/modules/manage-restaurant/manage-restaurant.module.ts
@@ -6,6 +6,9 @@ import { RestaurantCardComponent } from '../../components/restaurant-card/restau
 import { ManageRestaurantRoutingModule } from './manage-restaurant-routing.module';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
 @NgModule({
   declarations: [ManageRestaurantComponent],
   imports: [
@@ -14,6 +17,9 @@ import { MatIconModule } from '@angular/material/icon';
     ManageRestaurantRoutingModule,
     MatButtonModule,
     MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
   ],
 })
 export class ManageRestaurantModule {}
